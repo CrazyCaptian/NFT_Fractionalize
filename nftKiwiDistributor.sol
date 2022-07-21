7a2bc800731808f60bb27b8d94fec72d580601ba
Untouchable2k <dottage@gmail.com>
2022-07-20 22:58:57 -0500

    Update nftKiwiDistributor.sol

--- nftKiwiDistributor.sol
+++ nftKiwiDistributor.sol
@@ -66,41 +66,83 @@ contract Ownable2 {
 }
 
 contract forgeDistributeNFT is Ownable2 {
+
+	uint public totalMax = 12;
 	uint public totalAmt = 0;
 	uint public totalAmtKiwi = 0;
-    uint public totalTimes = 3;
+    uint public totalTimesForge = 0;
     uint public timestamp = block.timestamp;
+    uint public timestampKiwi = block.timestamp;
     uint public length = 60 * 10;
-    address public forgecontract = 0xF44fB43066F7ECC91058E3A614Fb8A15A2735276;
-    address public kiwicontract = 0xF44fB43066F7ECC91058E3A614Fb8A15A2735276;
+    uint public ForgePer = 0;
+    uint public KiwiPer = 0;
+    address public forgeContract = 0xF44fB43066F7ECC91058E3A614Fb8A15A2735276;
+    address public kiwiContract = 0xF44fB43066F7ECC91058E3A614Fb8A15A2735276;
     address public NFTFractionalizedContract  = 0xF44fB43066F7ECC91058E3A614Fb8A15A2735276;
+    
+    
     constructor() public {
-    
-    }
-    function mintToday() external {
-       require(timestamp < block.timestamp, "Timestamp must be less than current block");
 
-            timestamp = block.timestamp + length;
-            totalTimes = totalTimes + 1;
-            ERC20(NFTFractionalizedContract).transfer(forgecontract, totalAmt/totalTimes);
-            ERC20(kiwicontract).transfer(forgecontract, totalAmtKiwi/totalTimes);
+    }
     
+    
+    function mintTWO() external{
+         require(timestamp < block.timestamp && timestampKiwi < block.timestamp, "Timestamp must be less than current block");
+	mintForge();
+	mintKiwi();
+	
+	}
+		
+    function mintKiwi() external {
+        require(timestampKiwi < block.timestamp, "Timestamp must be less than current block");
+	require(totalTimesForge<totalMax, "Deposit to continue"){
+	timestampKiwi = block.timestamp + length;
+	totalTimesKiwi = totalTimes + 1;
+        ERC20(kiwicontract).transfer(forgecontract, KiwiPer);
+	totalAmtKiwi = totalAmtKiwi - (totalAmtKiwi / 12);
         // transfer the token from address of this contract
         // to address of the user (executing the withdrawToken() function)
-        
+    }
+    
+    
+    function mintForge() external {
+       require(timestamp < block.timestamp, "Timestamp must be less than current block");
+	require(totalTimesForge<totalMax){
+        timestamp = block.timestamp + length;
+        totalTimesForge = totalTimes + 1;
+        ERC20(NFTFractionalizedContract).transfer(forgecontract, ForgePer);
+        ERC20(kiwicontract).transfer(forgecontract, KiwiPer);
+    	totalAmt = totalAmt - (totalAmt / totalMax);
+	totalAmtKiwi = totalAmtKiwi - (totalAmtKiwi / totalMax);
+	
+        // transfer the token from address of this contract
+        // to address of the user (executing the withdrawToken() function)
+	
     }    
-    function deposit(uint256 _amount) external onlyOwner {
+    
+    
+    function deposit(uint256 _amount) external {
+        ERC20(NFTFractionalizedContract).transferFrom(msg.sender, forgecontract, _amount);
+	require(totalAMT > _amount,"Only if you add more");
         totalAmt = totalAmt + _amount;
-        ERC20(NFTFractionalizedContract).transfer(forgecontract, _amount/totalTimes);
+	ForgePer = ERC20(NFTFractionalizedContract).balanceOf(Address(this)) / 12;
+	
         // transfer the token from address of this contract
         // to address of the user (executing the withdrawToken() function)
         
+            totalTimesForge = 0;
     }    
-    function depositKiwi(uint256 _amount) external onlyOwner {
+    
+    
+    function depositKiwi(uint256 _amount) external {{
+        ERC20(kiwiContract).transferFrom(msg.sender, address(this), _amount);
+	require(totalAmtKiwi > _amount,"Only if you add more");
         totalAmtKiwi = totalAmtKiwi + _amount;
-        ERC20(kiwicontract).transfer(forgecontract, _amount/totalTimes);
-        // transfer the token from address of this contract
+	KiwiPer = ERC20(KiwiContract).balanceOf(Address(this)) / totalMax;
+        
+	// transfer the token from address of this contract
         // to address of the user (executing the withdrawToken() function)
         
+            totalTimesKiwi = 0;
     }
 }
